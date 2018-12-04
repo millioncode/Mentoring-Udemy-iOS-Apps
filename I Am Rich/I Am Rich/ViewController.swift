600c1855148fc070ea60299c4d0d6b657f7a749c
Aaron Cleveland <aaroncleveland@Aarons-MacBook-Pro.local>
2018-12-03 21:10:27 -0600

    Udemy iOS App #1

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Aaron Cleveland on 12/3/18.
+//  Copyright © 2018 Aaron Cleveland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
