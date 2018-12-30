afe92174982ce86d351088ac6a41d88d4838c09b
Aaron <toscleveland@gmail.com>
2018-12-29 18:27:32 -0600

    Added Xylophone Project

---
+++ Xylophone/Xylophone/ VIewController.swift
@@ -0,0 +1,30 @@
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController{
+    
+    var audioPlayer : AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch {
+            print(error)
+        }
+        audioPlayer.play()
+    }
+    
+  
+
+}
+
